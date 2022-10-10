fd3bb55e5d62b474f4f5232f469ec49a56793f91
VeronikaAleshkova <105292660+VeronikaAleshkova@users.noreply.github.com>
2022-10-10 21:43:11 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+// console.log(btnStart);
+// console.log(btnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const onStartClick = () => {
+  btnStart.disabled = true;
+  timerId = setInterval(() => {
+    let currentColor;
+    currentColor = getRandomHexColor();
+    body.style.backgroundColor = currentColor;
+  }, 1000);
+};
+
+const onStopClick = () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  };
+
+  btnStart.addEventListener('click', onStartClick);  
+  btnStop.addEventListener('click', onStopClick);  
 
